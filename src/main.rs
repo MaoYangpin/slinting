88b7bd0efa50e4d224f3109d37a313ab73de160d
MaoYangpin <mao.yangpin@gmail.com>
2025-04-02 13:49:19 +0800

    feat: add dynamic greeting with user input
    - Modify button click handler to accept text parameter
    - Add line-edit-text property to store user input
    - Update UI to pass user input to button click handler

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ slint::include_modules!();
 
 fn main() {
     let ui = MainWindow::new().unwrap();
-    ui.on_button_clicked(move || "Hello".into());
+    ui.on_button_clicked(move |text| format!("Hello {}", text).into());
     ui.run().unwrap();
 }
